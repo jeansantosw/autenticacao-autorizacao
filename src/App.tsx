faa51339fd2d9babd65771b932b3505b56dcc060
Jean Santos <jeansantos.sw@gmail.com>
2023-02-27 23:39:38 +0000

    autenticação

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { FormEvent, useState } from "react"
+
+
+export function App() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSubmit(event : FormEvent) {
+    event.preventDefault()
+    const data = {
+      email,
+      password
+    }
+    console.log(data)
+  }
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+      <button type="submit">Entrar</button>
+    </form>
+  )
+}
