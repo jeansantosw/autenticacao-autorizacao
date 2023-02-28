db5461156149b607097b8c2fda46f450c9b3edc4
Jean Santos <jeansantos.sw@gmail.com>
2023-02-28 00:01:02 +0000

    autenticação

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import { FormEvent, useState } from "react"
-
+import { FormEvent, useState } from 'react'
 
 export function App() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  function handleSubmit(event : FormEvent) {
+  function handleSubmit(event: FormEvent) {
     event.preventDefault()
     const data = {
       email,
-      password
+      password,
     }
     console.log(data)
   }
 
-
   return (
     <form onSubmit={handleSubmit}>
-      <input type="email" value={email} onChange={e => setEmail(e.target.value)}/>
-      <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button type="submit">Entrar</button>
     </form>
   )
